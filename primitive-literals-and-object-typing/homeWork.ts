84d09e95764d37fb0c7ea542d9bc5ea887472f82
Ihor Kosheliev <ihor.kosheliev@mev.com>
2023-10-27 22:48:34 +0300

    Implement new class hierarchy
    The old class hierarchy was commented out and a new one was implemented instead in src/index.ts file. The reasons for these changes are the enhancement of encapsulation, the removal of redundant code and the addition of singleton patterns. A new file "homeWork.ts" was created for better organization of code files.

---
+++ primitive-literals-and-object-typing/homeWork.ts
@@ -0,0 +1,133 @@
+class School {
+    private static instance: School;
+    private _areas: Map<string, Area> = new Map<string, Area>();
+    private _lecturers: Map<string, Lecturer> = new Map<string, Lecturer>();
+
+    private constructor() {}
+
+    public static getInstance(): School {
+        if (!School.instance) {
+            School.instance = new School();
+        }
+        return School.instance;
+    }
+
+    addArea(area: Area): void {
+        this._areas.set(area.name, area);
+    }
+
+    removeArea(areaName: string): void {
+        this._areas.delete(areaName);
+    }
+
+    addLecturer(lecturer: Lecturer): void {
+        this._lecturers.set(lecturer.name, lecturer);
+    }
+
+    removeLecturer(lecturerName: string): void {
+        this._lecturers.delete(lecturerName);
+    }
+}
+
+class Area {
+    private _levels: Map<string, Level> = new Map<string, Level>();
+    private readonly _name: string;
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    addLevel(level: Level): void {
+        this._levels.set(level.name, level);
+    }
+
+    removeLevel(levelName: string): void {
+        this._levels.delete(levelName);
+    }
+}
+
+class Level {
+    private _groups: Map<string, Group> = new Map<string, Group>();
+    private readonly _name: string;
+    private readonly _description: string;
+
+    constructor(name: string, description: string) {
+        this._name = name;
+        this._description = description;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    get description(): string {
+        return this._description;
+    }
+
+    addGroup(group: Group): void {
+        this._groups.set(group.name, group);
+    }
+
+    removeGroup(groupName: string): void {
+        this._groups.delete(groupName);
+    }
+}
+
+class Group {
+    private _students: Map<string, Student> = new Map<string, Student>();
+    private readonly _name: string;
+    private _directionName: string;
+    private _levelName: string;
+
+    constructor(name: string, directionName: string, levelName: string) {
+        this._name = name;
+        this._directionName = directionName;
+        this._levelName = levelName;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    addStudent(student: Student): void {
+        this._students.set(student.fullName, student);
+    }
+
+    removeStudent(fullName: string): void {
+        this._students.delete(fullName);
+    }
+}
+
+class Student {
+    private readonly _firstName: string;
+    private readonly _lastName: string;
+    private _grades: Map<string, number> = new Map<string, number>();
+    private _visits: Map<number, boolean> = new Map<number, boolean>();
+    private _birthYear: number;
+
+    constructor(firstName: string, lastName: string, birthYear: number) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._birthYear = birthYear;
+    }
+
+    get fullName(): string {
+        return `${this._firstName} ${this._lastName}`;
+    }
+}
+
+class Lecturer {
+    private readonly _name: string;
+
+    constructor(name: string) {
+        this._name = name;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,122 @@
-class School {
-    directions: Direction[] = [];
-
-    addDirection(direction: Direction): void {
-        this.directions.push(direction);
-    }
-}
-
-class Direction {
-    levels: Level[] = [];
-    private readonly _name: string;
-
-    get name(): string {
-        return this._name;
-    }
-
-    constructor(name: string) {
-        this._name = name;
-    }
-
-    addLevel(level: Level): void {
-        this.levels.push(level);
-    }
-}
-
-class Level {
-    groups: Group[] = [];
-    private readonly _name: string;
-    private readonly _program: string;
-
-    constructor(name: string, program: string) {
-        this._name = name;
-        this._program = program;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    get program(): string {
-        return this._program;
-    }
-
-    addGroup(group: Group): void {
-        this.groups.push(group);
-    }
-}
-
-class Group {
-    private _students: Student[] = [];
-    directionName: string;
-    levelName: string;
-
-    get students(): Student[] {
-        return this._students;
-    }
-
-    constructor(directionName: string, levelName: string) {
-        this.directionName = directionName;
-        this.levelName = levelName;
-    }
-
-    addStudent(student: Student): void {
-        this._students.push(student);
-    }
-
-    showPerformance(): Student[] {
-        return [...this.students].sort(
-            (a, b) => b.getPerformanceRating() - a.getPerformanceRating()
-        );
-    }
-}
-
-class Student {
-    grades: Record<string, number> = {};
-    attendance: boolean[] = [];
-    firstName: string;
-    lastName: string;
-    birthYear: number;
-
-    constructor(firstName: string, lastName: string, birthYear: number) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthYear = birthYear;
-    }
-
-    get fullName(): string {
-        return `${this.lastName} ${this.firstName}`;
-    }
-
-    set fullName(value: string) {
-        [this.lastName, this.firstName] = value.split(" ");
-    }
-
-    get age(): number {
-        return new Date().getFullYear() - this.birthYear;
-    }
-
-    setGrade(subject: string, grade: number): void {
-        this.grades[subject] = grade;
-    }
-
-    markAttendance(present: boolean): void {
-        this.attendance.push(present);
-    }
-
-    getPerformanceRating(): number {
-        const gradeValues = Object.values(this.grades);
-
-        if (gradeValues.length === 0) return 0;
-
-        const averageGrade =
-            gradeValues.reduce((sum, grade) => sum + grade, 0) / gradeValues.length;
-
-        const attendancePercentage =
-            (this.attendance.filter((present) => present).length /
-                this.attendance.length) *
-            100;
-
-        return (averageGrade + attendancePercentage) / 2;
-    }
-}
+// class School {
+//     directions: Direction[] = [];
+//
+//     addDirection(direction: Direction): void {
+//         this.directions.push(direction);
+//     }
+// }
+//
+// class Direction {
+//     levels: Level[] = [];
+//     private readonly _name: string;
+//
+//     get name(): string {
+//         return this._name;
+//     }
+//
+//     constructor(name: string) {
+//         this._name = name;
+//     }
+//
+//     addLevel(level: Level): void {
+//         this.levels.push(level);
+//     }
+// }
+//
+// class Level {
+//     groups: Group[] = [];
+//     private readonly _name: string;
+//     private readonly _program: string;
+//
+//     constructor(name: string, program: string) {
+//         this._name = name;
+//         this._program = program;
+//     }
+//
+//     get name(): string {
+//         return this._name;
+//     }
+//
+//     get program(): string {
+//         return this._program;
+//     }
+//
+//     addGroup(group: Group): void {
+//         this.groups.push(group);
+//     }
+// }
+//
+// class Group {
+//     private _students: Student[] = [];
+//     directionName: string;
+//     levelName: string;
+//
+//     get students(): Student[] {
+//         return this._students;
+//     }
+//
+//     constructor(directionName: string, levelName: string) {
+//         this.directionName = directionName;
+//         this.levelName = levelName;
+//     }
+//
+//     addStudent(student: Student): void {
+//         this._students.push(student);
+//     }
+//
+//     showPerformance(): Student[] {
+//         return [...this.students].sort(
+//             (a, b) => b.getPerformanceRating() - a.getPerformanceRating()
+//         );
+//     }
+// }
+//
+// class Student {
+//     grades: Record<string, number> = {};
+//     attendance: boolean[] = [];
+//     firstName: string;
+//     lastName: string;
+//     birthYear: number;
+//
+//     constructor(firstName: string, lastName: string, birthYear: number) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.birthYear = birthYear;
+//     }
+//
+//     get fullName(): string {
+//         return `${this.lastName} ${this.firstName}`;
+//     }
+//
+//     set fullName(value: string) {
+//         [this.lastName, this.firstName] = value.split(" ");
+//     }
+//
+//     get age(): number {
+//         return new Date().getFullYear() - this.birthYear;
+//     }
+//
+//     setGrade(subject: string, grade: number): void {
+//         this.grades[subject] = grade;
+//     }
+//
+//     markAttendance(present: boolean): void {
+//         this.attendance.push(present);
+//     }
+//
+//     getPerformanceRating(): number {
+//         const gradeValues = Object.values(this.grades);
+//
+//         if (gradeValues.length === 0) return 0;
+//
+//         const averageGrade =
+//             gradeValues.reduce((sum, grade) => sum + grade, 0) / gradeValues.length;
+//
+//         const attendancePercentage =
+//             (this.attendance.filter((present) => present).length /
+//                 this.attendance.length) *
+//             100;
+//
+//         return (averageGrade + attendancePercentage) / 2;
+//     }
+// }

a875b424ec233342300a499497c24fcc0c2002b6
Ihor Kosheliev <ihor.kosheliev@mev.com>
2023-11-14 17:34:02 +0200

    Add Shape abstract class and subclasses
    This commit introduces an abstract class, Shape, and four subclasses: Circle, Rectangle, Square, and Triangle. Each subclass has its own implementation of the calculateArea method inherited from the Shape abstract class.

---
+++ abstract_class/homeWork.ts
@@ -0,0 +1,69 @@
+abstract class Shape {
+    readonly name: string;
+    readonly color: string;
+
+    protected constructor(name: string, color: string) {
+        this.name = name;
+        this.color = color;
+    }
+
+    abstract calculateArea(): number;
+}
+
+class Circle extends Shape {
+    radius: number;
+
+    constructor(name: string, color: string, radius: number) {
+        super(name, color);
+        this.radius = radius;
+    }
+
+    calculateArea(): number {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
+
+class Rectangle extends Shape {
+    width: number;
+    height: number;
+
+    constructor(name: string, color: string, width: number, height: number) {
+        super(name, color);
+        this.width = width;
+        this.height = height;
+    }
+
+    calculateArea(): number {
+        return this.width * this.height;
+    }
+
+    print(): void {
+        console.log(`Rectangle Area = width * height`);
+    }
+}
+
+class Square extends Rectangle {
+
+    constructor(name: string, color: string, side: number) {
+        super(name, color, side, side);
+    }
+
+    print(): void {
+        console.log(`Square Area = side * side`);
+    }
+}
+
+class Triangle extends Shape {
+    base: number;
+    height: number;
+
+    constructor(name: string, color: string, base: number, height: number) {
+        super(name, color);
+        this.base = base;
+        this.height = height;
+    }
+
+    calculateArea(): number {
+        return 0.5 * this.base * this.height;
+    }
+}

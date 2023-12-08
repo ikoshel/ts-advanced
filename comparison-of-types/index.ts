a5c25b389c9f16bb20418289a5901c385ce63c87
Ihor Kosheliev <ihor.kosheliev@mev.com>
2023-12-08 17:07:49 +0200

    Add UpperCaseKeys type and apply to User
    A new type, UpperCaseKeys, has been added to the TypeScript utility types in comparison-of-types/index.ts. This new type converts the keys of a given object to uppercase. It has been applied to the User interface as a demonstration of its usage.

--- comparison-of-types/index.ts
+++ comparison-of-types/index.ts
@@ -6,17 +6,19 @@ type DeepReadonly<T> = {
 // Task 2
 type DeepRequireReadonly<T> = {
     readonly [K in keyof Required<T>]: Required<T>[K] extends Record<string, unknown>
-        ? DeepRequireReadonly<Required<T>[K]> : Required<T>[K];
+        ? DeepRequireReadonly<Required<T>[K]>: Required<T>[K];
 }
 
 // Task 3
+type UpperCaseKeys<T> = {
+    [K in keyof T as Uppercase<string & K>]: T[K]
+}
 
 // Task 4
 type TPick<T, K extends keyof T> = {
     [P in K]: T[P];
 }
 
-// Testing for new types
 interface User {
     name: string;
     age: number;
@@ -25,4 +27,5 @@ interface User {
 
 type MyReadonlyUser = DeepReadonly<User>;
 type MyDeepRequireReadonlyUser = DeepRequireReadonly<User>;
+type MyUpperCaseKeysUser = UpperCaseKeys<User>;
 type MyPickUser = TPick<User, 'name' | 'age'>;
